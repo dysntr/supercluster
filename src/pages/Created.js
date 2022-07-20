6ee02782a81c9ace99747bdfb4b515d6df6959e9
user <email@example.com>
2022-07-20 00:05:50 -0300

    fixed checkMessages

--- src/pages/Created.js
+++ src/pages/Created.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { getTodayDate, colorLog } from "../utils/Misc";
 
@@ -24,7 +24,7 @@ export default function Created(props) {
     setSubject("");
     setEncryptionKey("secret");
     setCommand("");
-  }
+  };
 
   const sendMessage = async (message) => {
     try {
@@ -35,11 +35,11 @@ export default function Created(props) {
         const conversation = await currentXMTP.conversations.newConversation(
           "0xd69DFe5AE027B4912E384B821afeB946592fb648"
         );
-        await conversation.send(message);
+        await conversation.send(JSON.stringify(message));
 
-        colorLog(2, "Sending message to user", message);
+        colorLog(2, "Sending message to user", JSON.stringify(message));
       } else {
-        console.error("Current XMTP is not available in Created component")
+        console.error("Current XMTP is not available in Created component");
       }
       colorLog(1, "Exiting sendMessage");
     } catch (error) {
@@ -48,32 +48,51 @@ export default function Created(props) {
   };
 
   const submitMessage = async () => {
-
     let messageObject = {
       command: command,
       cid: cid,
       subject: subject,
       createdDate: getTodayDate(),
-      encryptionKey: {},
-    }
+      encryptionKey: encryptionKey,
+    };
 
     try {
       await sendMessage(messageObject);
-    } catch(e) {
-      console.error("Error sending message:", e)
+    } catch (e) {
+      console.error("Error sending message:", e);
     }
 
     resetState();
-    console.log(messageObject);
-  }
+    //console.log(messageObject);
+  };
 
   return (
     <>
-      <input placeholder="Please enter your recipient address" value={recipient} onChange={(e) => setRecipient(e.target.value)} />
-      <input placeholder="Please enter your CID" value={cid} onChange={(e) => setCid(e.target.value)} />
-      <input placeholder="Please enter a subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
-      <input placeholder="Please enter pin or unpin command" value={command} onChange={(e) => setCommand(e.target.value)} />
-      <input placeholder="Please enter an encryption key" value={encryptionKey} onChange={(e) => setEncryptionKey(e.target.value)} />
+      <input
+        placeholder="Please enter your recipient address"
+        value={recipient}
+        onChange={(e) => setRecipient(e.target.value)}
+      />
+      <input
+        placeholder="Please enter your CID"
+        value={cid}
+        onChange={(e) => setCid(e.target.value)}
+      />
+      <input
+        placeholder="Please enter a subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+      <input
+        placeholder="Please enter pin or unpin command"
+        value={command}
+        onChange={(e) => setCommand(e.target.value)}
+      />
+      <input
+        placeholder="Please enter an encryption key"
+        value={encryptionKey}
+        onChange={(e) => setEncryptionKey(e.target.value)}
+      />
       <button onClick={() => submitMessage()}>Submit</button>
     </>
   );
