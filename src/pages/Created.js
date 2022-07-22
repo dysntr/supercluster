edbbc57ee571e213eb8cdb52205200687a3f4bde
user <email@example.com>
2022-07-22 19:59:11 -0300

    Added functionality for viewing multiple NFTs

--- src/pages/Created.js
+++ src/pages/Created.js
@@ -3,50 +3,10 @@ import { colorLog } from "../utils/Misc.js";
 import NFTList from "../components/NFTs/NFTList.jsx";
 import SectionLoading from "../components/SectionLoading.jsx";
 
-export default function Created({ walletAddress, web3Api }) {
-  const [nfts, setNfts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function getCreatedNFTs(Web3Api, walletAddress) {
-      walletAddress = "0xd69DFe5AE027B4912E384B821afeB946592fb648";
-      const options = {
-        address: walletAddress,
-        chain: "polygon",
-      };
-
-      let createdNFTs = [];
-
-      const NFTs = await Web3Api.account.getNFTs(options);
-      if (NFTs.result.length > 0) {
-        NFTs.result.forEach((result) => {
-          let nft_metadata = result.metadata;
-
-          if (nft_metadata && nft_metadata.includes(walletAddress)) {
-            const metaData = JSON.parse(nft_metadata);
-            const trustedAddr = metaData.attributes[0].value;
-            metaData["trustedAddr"] = trustedAddr;
-
-            //removes duplicate attributes section
-            delete metaData.attributes;
-            console.log(metaData);
-            createdNFTs.push(metaData);
-          }
-        });
-        setIsLoading(false);
-        setNfts(createdNFTs);
-      } else {
-        console.log(colorLog(1, "Error retreiving created NFTs"));
-      }
-    }
-    getCreatedNFTs(web3Api, walletAddress);
-  }, [web3Api, walletAddress]);
-
-  console.log("Created Page", nfts);
-
-  if (isLoading) {
+export default function Created({ NFTsArray }) {
+  if (!NFTsArray) {
     return <SectionLoading />;
   }
 
-  return <NFTList nftList={nfts} isCreator={true} />;
+  return <NFTList nftList={NFTsArray} isCreatedPage={true} />;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,52 +3,10 @@ import React, { useEffect, useState } from "react";
 import { colorLog, fillNftArrayWithTestData } from "../utils/Misc";
 import SectionLoading from "../components/SectionLoading.jsx";
 
-export default function Home({
-  userNFTs,
-  allMessages,
-  walletAddress,
-  web3Api,
-}) {
-  const [nfts, setNfts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    async function getCreatedNFTs(Web3Api, walletAddress) {
-      const superClusterNFT = "0xd69DFe5AE027B4912E384B821afeB946592fb648";
-      const options = {
-        address: walletAddress,
-        chain: "polygon",
-      };
-
-      let createdNFTs = [];
-
-      const NFTs = await Web3Api.account.getNFTs(options);
-      if (NFTs.result.length > 0) {
-        NFTs.result.forEach((result) => {
-          let nft_metadata = result.metadata;
-
-          if (nft_metadata && nft_metadata.includes(superClusterNFT)) {
-            const metaData = JSON.parse(nft_metadata);
-            const trustedAddr = metaData.attributes[0].value;
-            metaData["trustedAddr"] = trustedAddr;
-
-            //removes duplicate attributes section
-            delete metaData.attributes;
-            console.log(metaData);
-            createdNFTs.push(metaData);
-          }
-        });
-        setIsLoading(false);
-        setNfts(createdNFTs);
-      } else {
-        console.log(colorLog(1, "Error retrieving created NFTs"));
-      }
-    }
-    getCreatedNFTs(web3Api, walletAddress);
-  }, [web3Api, walletAddress]);
-
-  if (isLoading) {
+export default function Home({ NFTsArray }) {
+  if (!NFTsArray) {
     return <SectionLoading />;
   }
-  return <NFTList nftList={nfts} isCreator={false} />;
+
+  return <NFTList nftList={NFTsArray} isCreator={false} />;
 }
